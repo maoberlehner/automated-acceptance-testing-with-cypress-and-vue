daff2614e1432f575298414e912698ab0fe3f39f
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-03-11 06:34:59 +0100

    Show user data in the header

--- src/components/page/PageHome.vue
+++ src/components/page/PageHome.vue
@@ -1,11 +1,36 @@
 <template>
   <div :class="$options.name">
-    <h1 data-qa="main headline">Home</h1>
+    <header data-qa="header">
+      <p v-if="user" data-qa="username">{{ user.username }}</p>
+    </header>
+    <main>
+      <h1 data-qa="main headline">Home</h1>
+    </main>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+// This would usually come from a cookie or the local storage.
+const currentUserId = 1;
+
 export default {
   name: `PageHome`,
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    this.fetchUser(currentUserId);
+  },
+  methods: {
+    async fetchUser(id) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+      this.user = response.data;
+    },
+  },
 };
 </script>
--- test/features/home.js
+++ test/features/home.js
@@ -1,14 +1,21 @@
 describe(`Home`, () => {
-  beforeEach(() => {
-    // Because we're only testing the homepage
-    // in this test file, we can run this command
-    // before each individual test instead of
-    // repeating it in every test.
-    cy.visit(`/`);
-  });
-
   it(`Should display the main headline.`, () => {
+    cy.visit(`/`);
     cy.get(`[data-qa="main headline"]`).should(`be.visible`);
     cy.get(`[data-qa="main headline"]`).contains(`Home`);
+  });
+
+  it(`Should display the name of the curent user in the header.`, () => {
+    // Enable response stubbing.
+    cy.server();
+    // Route the API request to a stubbed response.
+    cy.route({
+      method: `GET`,
+      url: `/users/*`,
+      response: { username: `JohnDoe` },
+    });
+    cy.visit(`/`);
+
+    cy.get(`[data-qa="header"] [data-qa="username"]`).should(`contain`, `JohnDoe`);
   });
 });
